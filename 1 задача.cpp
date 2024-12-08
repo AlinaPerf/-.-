de5105adf998082525814597cb624d021a0737ca
AlinaPerf <alinaperfileva44@gmail.com>
2024-12-08 22:28:44 +0300

    Add files via upload

---
+++ 1 задача.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+
+
+int main()
+{
+	int sum{};
+	for (int i = 2; i <= 1000; i++) 
+	{
+	 if (i % 3 == 0 || i % 5 == 0)
+	  sum += i;
+
+	}
+
+	std::cout << sum;
+	return 0;
+}
