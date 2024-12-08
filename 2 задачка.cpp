ad8bcf360ec666b2fb8e826e8dff5971e1a0cf87
AlinaPerf <alinaperfileva44@gmail.com>
2024-12-08 22:29:13 +0300

    Add files via upload

---
+++ 2 задачка.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+int main()
+{
+
+	int first = 1;
+	int second = 2;
+	int third = first + second;
+	int sum = 0;
+	while (third < 4000000)
+	{
+	 if (third % 2 == 0)
+	 {
+	  sum += third;
+	 }
+	 first = second;
+	 second = third;
+	 third = first + second;
+	}
+	std::cout << sum + 2;
+		return 0;
+}
